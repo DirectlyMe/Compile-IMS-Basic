c7f9af0753887cad70881d1bbc8db0ffafb102aa
Jack Hansen <hansenja2011@gmail.com>
2019-04-19 17:01:43 -0600

    Can connect to server over ssh

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as node_ssh from "node-ssh";
+import { connectToServer } from "./commands/registerWithServer";
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,17 +11,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "compile-ims-basic" is now active!');
+	console.log('Congratulations, your extension "compile-ims-basic" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	let disposable = vscode.commands.registerCommand('extension.connectToServer', () => connectToServer());
 
 	context.subscriptions.push(disposable);
 }
