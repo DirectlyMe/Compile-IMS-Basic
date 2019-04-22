1891b094a54b8b61d920d9cbb929b59d0a30c517
John Moss <johnm@dakcs.com>
2019-04-22 09:59:41 -0600

    Added smart directory checking to compile and run

--- src/commands/compileFile.ts
+++ src/commands/compileFile.ts
@@ -7,47 +7,46 @@ export async function compileFile(uri: vscode.Uri) {
     // needs all three in order to proceed
     if (!uri.authority || uri.scheme !== "ssh" || !uri.fsPath) return;
 
-    // get just the file name
+try {
+    const connection = getConnectionConfig(uri.authority);
+    if (!connection) return;
+
+    const session = await establishConnection(connection);
+    if (!session) return;
+
+    // Check if there is a root directory
+    const root_dir = connection.root === undefined? "": connection.root;
+
+    // Check for a src folder in the file path selected
+    const src_pos = uri.path.lastIndexOf("/src");
+    if (src_pos <= 0)
+    {
+        console.log("File not in a src directory.");
+        return;
+    }
+
+    // Get the working directory for the compiler
+    const work_dir = root_dir + uri.path.substring(
+        0,
+        src_pos
+    );
+
+    // Get the file name
     const file = uri.path.substring(
-        uri.path.lastIndexOf("/") + 1,
+        src_pos + 5,
         uri.path.length
     );
 
-    // check if the file is in the root directory
-    const filePath =
-    "/" + file === uri.path 
-        ? "." + uri.path
-        : uri.path;
-
-    try {
-        const connection = getConnectionConfig(uri.authority);
-        if (!connection) return;
-
-        const session = await establishConnection(connection);
-        if (!session) return;
-
-
-        // check if we're already in /u/sting/src/3
-        if (uri.path !== `/u/sting/src/3/${file}`) {
-            const { stdout, stderr } = await session.execCommand(
-                `cp ${filePath} /u/sting/src/3`,
-                {}
-            );
-            console.log(stdout);
-            console.log(stderr);
-
-            // if stderr returned something our file copy failed
-            if (stderr !== "") return;
-        }
-
-        // bring in environment variables, cd into /u/sting and compile the script
-        const { stdout, stderr } = await session.execCommand(
-            `. /etc/setdakcsenv; BASIC -C 3/${file}`,
-            { cwd: "/u/sting" }
-        );
-        console.log(stdout);
-        console.log(stderr);
-    } catch (error) {
+    // bring in environment variables, cd into working directory, compile program
+    const { stdout, stderr } = await session.execCommand(
+        `. /etc/setdakcsenv; BASIC -C ${file}`,
+        { cwd: `${work_dir}` }
+    );
+    console.log(stdout);
+    console.log(stderr);
+    } 
+    
+    catch (error) {
         console.error(error);
     }
 }
--- src/commands/runFile.ts
+++ src/commands/runFile.ts
@@ -4,17 +4,9 @@ import {
     getConnectionConfig,
     establishConnection
 } from "../util/connectionUtils";
+import { stringify } from "querystring";
 
 export async function runFile(uri: vscode.Uri) {
-    // needs all three in order to proceed
-    if (!uri.authority || uri.scheme !== "ssh" || !uri.fsPath) return;
-
-    // get just the file name
-    const file = uri.path.substring(
-        uri.path.lastIndexOf("/") + 1,
-        uri.path.length
-    );
-
     try {
         const connection = getConnectionConfig(uri.authority);
         if (!connection) return;
@@ -22,8 +14,43 @@ export async function runFile(uri: vscode.Uri) {
         const session = await establishConnection(connection);
         if (!session) return;
 
-        session.execCommand(`. /etc/setdakcsenv; RUN 3/${file}`, {
-            cwd: "/u/sting",
+        // Check for a src folder in the file path selected
+        const src_pos = uri.path.lastIndexOf("/src");
+
+        let file;
+        let work_dir;
+        let root_dir;
+
+        // Check if there is a root directory
+        root_dir = connection.root === undefined? "": connection.root;
+
+        // File path 
+        if (src_pos <= 0)
+        {
+            const second_last_index = uri.path.substring(0, uri.path.lastIndexOf("/")).lastIndexOf("/");
+            work_dir = root_dir + uri.path.substring(
+                0,
+                second_last_index
+            );
+            file = uri.path.substring(second_last_index + 1, uri.path.length);
+        }
+        else
+        {
+            // Get the working directory for the compiler
+            work_dir = root_dir + uri.path.substring(
+                0,
+                src_pos
+            );
+
+            // Get the file name
+            file = uri.path.substring(
+                src_pos + 5,
+                uri.path.length
+            );
+        }
+
+        session.execCommand(`. /etc/setdakcsenv; RUN ${file}`, {
+            cwd: `${work_dir}`,
             onStdout(chunk: Buffer) {
                 console.log(chunk.toString("utf8"));
             },
